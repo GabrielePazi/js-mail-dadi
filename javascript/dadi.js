a3f334361c1ac5a6c43a46d48e93f01ef82f10a6
GabrielePazi <pazigabriele@gmail.com>
2023-07-05 19:16:40 +0200

    1.2 gestione errore da input e stampa i html

--- javascript/dadi.js
+++ javascript/dadi.js
@@ -1,5 +1,6 @@
 const btnSubmit = document.querySelector(".btn-submit");
 const inputNumber = document.querySelector(".user-number");
+const gameContainer = document.querySelector(".box")
 let userNumbers = [];
 let botNumbers = [];
 let userSum = 0;
@@ -11,24 +12,44 @@ btnSubmit.addEventListener("click", function() {
     //check della lunghezza dell'array dei numeri dell'utente
     if (userNumbers.length < 3) {
       number = inputNumber.value; 
-      userNumbers.push(number); //carica il number inserito da utente nell'array
+      if (number > 6 || number < 1) {//controlla che l'input sia valido
+        let error = document.createElement("h2");
 
-      userSum += parseInt(number); //calcola la somma dei numeri dell'utente 
+        error.classList.add("text-danger")
+        error.innerHTML = "Hai inserito un numero non valido! Ricarica la pagina per riprovare"
 
-      botNumbers.push(botNumber) //aggiungo i numeri random all'array botNumbers
+        gameContainer.append(error);
+      } else {
+        userNumbers.push(number); //carica il number inserito da utente nell'array
 
-      botSum += botNumber; //faccio la somma dei numeri random
+        userSum += parseInt(number); //calcola la somma dei numeri dell'utente 
+
+        botNumbers.push(botNumber) //aggiungo i numeri random all'array botNumbers
+
+        botSum += botNumber; //faccio la somma dei numeri random
+      }
     }
     
     if (userNumbers.length == 3) { //quando l'utente inserisce 3 numeri mi stampa in console il confronto
-      console.log(userNumbers, botNumbers, userSum, botSum);
-
       if (botSum > userSum) {
-        console.log("Peccato")
+        let result = document.createElement("h3");
+
+        result.innerHTML = "Hei perso, il tuo punteggio è: " + userSum + " e il punteggio del tuo avversario è: " + botSum;
+
+        gameContainer.append(result);
       } else if (botSum < userSum) {
-        console.log("Congratulazioni")
+        let result = document.createElement("h3");
+
+        result.innerHTML = "Hai vinto, il tuo punteggio è: " + userSum + " e il punteggio del tuo avversario è: " + botSum;
+
+        gameContainer.append(result);
+
       } else if (botSum = userSum){
-        console.log("Pareggio");
+        let result = document.createElement("h3");
+
+        result.innerHTML = "Pareggio, il tuo punteggio è: " + userSum + " e il punteggio del tuo avversario è: " + botSum;
+
+        gameContainer.append(result);
       }
     }
     inputNumber.value = ""; //resetto l'input in html
