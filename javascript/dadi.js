500d82f2b9fe552d15a42faf253310e548e094cd
GabrielePazi <pazigabriele@gmail.com>
2023-07-05 18:51:10 +0200

    1.1.1 modifica nomi variabili

--- javascript/dadi.js
+++ javascript/dadi.js
@@ -1,12 +1,12 @@
 const btnSubmit = document.querySelector(".btn-submit");
 const inputNumber = document.querySelector(".user-number");
 let userNumbers = [];
-let npcNumbers = [];
+let botNumbers = [];
 let userSum = 0;
-let npcSum = 0;
+let botSum = 0;
 
 btnSubmit.addEventListener("click", function() {
-    let npcNumber = Math.floor((Math.random() * 6) + 1);
+    let botNumber = Math.floor((Math.random() * 6) + 1);
 
     //check della lunghezza dell'array dei numeri dell'utente
     if (userNumbers.length < 3) {
@@ -15,19 +15,19 @@ btnSubmit.addEventListener("click", function() {
 
       userSum += parseInt(number); //calcola la somma dei numeri dell'utente 
 
-      npcNumbers.push(npcNumber) //aggiungo i numeri random all'array npcNumbers
+      botNumbers.push(botNumber) //aggiungo i numeri random all'array botNumbers
 
-      npcSum += npcNumber; //faccio la somma dei numeri random
+      botSum += botNumber; //faccio la somma dei numeri random
     }
     
     if (userNumbers.length == 3) { //quando l'utente inserisce 3 numeri mi stampa in console il confronto
-      console.log(userNumbers, npcNumbers, userSum, npcSum);
+      console.log(userNumbers, botNumbers, userSum, botSum);
 
-      if (npcSum > userSum) {
+      if (botSum > userSum) {
         console.log("Peccato")
-      } else if (npcSum < userSum) {
+      } else if (botSum < userSum) {
         console.log("Congratulazioni")
-      } else if (npcSum = userSum){
+      } else if (botSum = userSum){
         console.log("Pareggio");
       }
     }
